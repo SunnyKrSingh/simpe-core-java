8c25526645aae01224c03a187922840768ecd666
sunny <sunny123@gmail.com>
2019-07-09 18:25:42 +0530

    update one line

--- Simple.java
+++ Simple.java
@@ -2,5 +2,6 @@ public class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
 	 System.out.println("Second line added");  
+	 System.out.println("third line added");  
     }  
 } 
